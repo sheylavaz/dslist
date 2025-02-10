adfd3344826861317888381ec4c4a2fc260bb10a
sheylavaz <sheylavaz@gmail.com>
2025-02-10 18:26:42 -0300

    Spring Data JPA, Relacionamento N-N, classe associação, embedded id, consulta SQL, Projections

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -1,11 +1,26 @@
 package com.devsuperior.dslist.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 //camada de acesso aos dados, já faz varias requisições no banco
 
 public interface GameRepository extends JpaRepository<Game, Long>{
-
+	
+	//consulta SQL não é JPQL, por isso precisa de uma interface 
+	@Query(nativeQuery = true, value = """
+			SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+			tb_game.short_description AS shortDescription, tb_belonging.position
+			FROM tb_game
+			INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
+			WHERE tb_belonging.list_id = :listId
+			ORDER BY tb_belonging.position
+				""")
+	List<GameMinProjection> searchByList(Long listId);
+	
 }
